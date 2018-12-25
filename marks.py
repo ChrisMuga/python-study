627393730d1aa0e4415dbdb8a3973e30d8ef4ee2
Chris Muga <chrismuga94@gmail.com>
2018-12-25 15:02:42 +0300

    just sprucing up

--- marks.py
+++ marks.py
@@ -1,6 +1,7 @@
+
 def assignment():
-    teacher             =   input('Enter teacher name: ')
-    number_of_students  =   int(input('Enter the class population: '))
+    teacher              =   input('Enter teacher name: ')
+    number_of_students   =   int(input('Enter the class population: '))
     data_entry(teacher,number_of_students)
 
 
@@ -12,12 +13,29 @@ def data_entry(teacher,number_of_students):
         student_name    =   input(f'Enter Student Name {counter+1}/{number_of_students}: ')
 
         #append name to student's list
-        students.insert(student_name)
+        students.append(student_name)
 
         #increment counter
         counter +=  1
 
     print(students)
+
+
+def subjects(students, number_of_students):
+
+    counter     =   0
+
+    while(counter < number_of_students):
+    
+        student_name    =   input(f'Enter Student Name {counter+1}/{number_of_students}: ')
+
+        #append name to student's list
+        students.append(student_name)
+
+        #increment counter
+        counter +=  1
+
+
 
     
 assignment()
