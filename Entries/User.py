51137f0c2b5b137279aee0a457add0fef59b7855
chrismuga <ChrisMuga94@gmail.com>
2019-09-29 11:22:39 +0300

    Add alias to User class

--- Entries/User.py
+++ Entries/User.py
@@ -1,12 +1,14 @@
 class User:
     def __init__(self, **kwargs):
         self.name = kwargs.get("name")
+        self.alias = kwargs.get("alias")
 
     def display_info(self):
         # convert object to dictionary using __dict__ method and print
         print(self.__dict__)
         # obtain variables object using get 
-        print(self.name)
+        print("Name:", self.name)
+        print("Alias:", self.alias)
 
-user = User(name = "Christian Bale")
+user = User(name = "Christian Bale", alias = "Batman")
 user.display_info()
