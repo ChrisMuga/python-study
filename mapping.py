56a3f19d0874c53d96581d063f13e987f8822265
chrismuga <ChrisMuga94@gmail.com>
2019-10-06 16:14:44 +0300

    mapping #2

--- mapping.py
+++ mapping.py
@@ -1,3 +1,4 @@
+# 1
 names = [
     "chris",
     "muga",
@@ -6,3 +7,21 @@ names = [
 ]
 
 list(map(lambda x: print(x.upper()), names))
+
+# 2
+people = [
+    {
+        "name": "Chris Muga",
+        "age": 23
+    },
+    {
+        "name": "Donna Sonia",
+        "age": 21
+    },
+    {
+        "name": "Nicole Andang'o",
+        "age": 17
+    }
+]
+
+list(map(lambda x: print(x.get("name")), people))
